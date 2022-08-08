647ecc78de996caf89e395c759eca79eaa556d45
Yash Mathur <yashmthr65@gmail.com>
2022-08-08 23:59:06 +0530

    Layout Added

--- cryptoapp/src/App.js
+++ cryptoapp/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Switch,Route,Link } from 'react-router-dom';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-
-
+import { Navbar,Exchanges,Homepage,Cryptodetails,Cryptocurrencies,News } from './components';
+import "./App.css";
 const App=()=>{
     return (
         <div className='app'>
             <div className='navbar'>
-
+                <Navbar></Navbar>
             </div>
             <div className='main'>
-
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path="/" element={<Homepage/>}/>
+                            <Route path="/exchanges" element={<Exchanges/>}/>
+                            <Route path="/crpytocurrencies" element={<Cryptocurrencies/>}/>
+                            <Route path="/crpyto/:coinId" element={<Cryptodetails />}/>
+                            <Route path="/news" element={<News/>}/>
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
             <div className='footer'>
-                
+
             </div>
             
         </div>
--- cryptoapp/src/index.js
+++ cryptoapp/src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import 'antd/dist/antd.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<Router><App></App></Router>);
