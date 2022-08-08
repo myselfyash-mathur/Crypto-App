3f148d81871a0aa677ad11769948e03df6a18909
Yash Mathur <yashmthr65@gmail.com>
2022-08-09 00:06:25 +0530

    Complete Layout

--- cryptoapp/src/App.js
+++ cryptoapp/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar,Exchanges,Homepage,Cryptodetails,Cryptocurrencies,News } from './components';
 import "./App.css";
+import { SpaceContext } from 'antd/lib/space';
 const App=()=>{
     return (
         <div className='app'>
@@ -15,17 +16,26 @@ const App=()=>{
                         <Routes>
                             <Route exact path="/" element={<Homepage/>}/>
                             <Route path="/exchanges" element={<Exchanges/>}/>
-                            <Route path="/crpytocurrencies" element={<Cryptocurrencies/>}/>
-                            <Route path="/crpyto/:coinId" element={<Cryptodetails />}/>
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies/>}/>
+                            <Route path="/crypto/:coinId" element={<Cryptodetails />}/>
                             <Route path="/news" element={<News/>}/>
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-            <div className='footer'>
-
-            </div>
             
+                <div className='footer'>
+                    <Typography.Title  level={5} style={{color:'white', textAlign: 'center'}}>
+                        Cryptoverse <br/>
+                        All rights reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to="/">Home</Link>
+                        <Link to="/exchanges">Exchanges</Link>
+                        <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                        <Link to="/news">News</Link>
+                    </Space>
+                </div>
+            </div>
         </div>
     )
 }
